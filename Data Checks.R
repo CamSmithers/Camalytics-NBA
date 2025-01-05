fd77a8400258017ec280f23e0d28373a1ce06190
Cam <camalytics@outlook.com>
2025-01-05 14:38:17 -0500

    Jan 5th, 2024

--- Data Checks.R
+++ Data Checks.R
@@ -1,6 +1,5 @@
 # Checking Data Script
 data_check <- all_team_data %>%
-    select(team_team, team_opponent, team_date, 
-           team_cummean_offrating, team_cummean_defrating, 
-           opponent_cummean_offrating, opponent_cummean_defrating)
+    dplyr::select(team_team, team_opponent, team_date, 
+           close_win, close_loss, blowout_win, blowout_loss)
 View(data_check)
