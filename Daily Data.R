391ed611aee29b974c43a9f4b8406c8530beb6fa
Cam <camalytics@outlook.com>
2025-01-07 17:44:18 -0500

    Jan 7th, 2025

--- Daily Data.R
+++ Daily Data.R
@@ -1,3 +1,4 @@
+#Outcome 1 (Primary): Game Outcome (w/ Magnitude)
 daily.ppg_oppg_data <- ppg_oppg_data %>%
     dplyr::select(team_date, team_team, team_opponent, 
                   close_win, close_loss, blowout_win, blowout_loss) %>%
@@ -8,11 +9,12 @@ daily.offrtg_defrtg_data <- offrtg_defrtg_data %>%
                   close_win, close_loss, blowout_win, blowout_loss) %>%
     filter(team_date == Sys.Date())
 
-test.offrtg_defrtg_data <- offrtg_defrtg_data %>%
-    dplyr::select(team_date, team_team, team_opponent,
-                  close_win, close_loss, blowout_win, blowout_loss,
-                  team_game_outcome)
-
 View(daily.ppg_oppg_data)
 View(daily.offrtg_defrtg_data)
-View(test.offrtg_defrtg_data)
+
+#Outcomes 2 to 4
+daily.predictions <- all_team_data %>%
+    dplyr::select(team_date, team_team, team_opponent,
+                  team_win_chance, team_blowout_chance, team_upset_chance) %>%
+    filter(team_date == Sys.Date())
+View(daily.predictions)
--- Data Checks.R
+++ Data Checks.R
@@ -1,6 +1,5 @@
 # Checking Data Script
 data_check <- all_team_data %>%
-    dplyr::select(team_team, team_date, team_opponent, team_date, 
-           close_win, close_loss, blowout_win, blowout_loss) %>%
-    filter(team_date == Sys.Date())
+    dplyr::select() %>%
+    filter( == )
 View(data_check)
