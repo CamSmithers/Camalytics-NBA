83c98d6c1ab8fb5c9e6c4cba3cd72476732041a8
Cam <camalytics@outlook.com>
2025-01-09 20:28:05 -0500

    Jan 9th, 2025

--- Daily Data.R
+++ Daily Data.R
@@ -1,21 +1,27 @@
-#Outcome 1 (Primary): Game Outcome (w/ Magnitude)
-daily.ppg_oppg_data <- ppg_oppg_data %>%
-    dplyr::select(team_date, team_team, team_opponent, 
-                  close_win, close_loss, blowout_win, blowout_loss) %>%
-    filter(team_date == Sys.Date())
-
-daily.offrtg_defrtg_data <- offrtg_defrtg_data %>%
-    dplyr::select(team_date, team_team, team_opponent, 
-                  close_win, close_loss, blowout_win, blowout_loss) %>%
-    filter(team_date == Sys.Date())
-
-View(daily.ppg_oppg_data)
-View(daily.offrtg_defrtg_data)
-
-#Outcomes 2 to 4
+#Outcomes 2: Daily Predictions
 daily.predictions <- all_team_data %>%
     dplyr::select(team_date, team_team, team_opponent,
                   team_win_chance, team_cumrate_win,
                   team_upset_chance, team_cumrate_upset) %>%
-    filter(team_date == Sys.Date())
+    rename("Game Date"="team_date", "Team"="team_team", "Opponent"="team_opponent",
+           "Win Probability"="team_win_chance", "Current Win Pct"="team_cumrate_win", 
+           "Upset Probability"="team_upset_chance", "Current Upset Pct"="team_cumrate_upset") %>%
+    filter(`Game Date` == Sys.Date())
 View(daily.predictions)
+
+current_date <- format(Sys.Date(), "%m_%d_%y")
+
+#write_csv(daily.predictions, 
+#          paste0("/Users/camsmithers/Desktop/NBA Project/Main/Model Predictions/Game Outcome 2/Daily/go2_", current_date, ".csv"))
+
+#Outcome 2: Historical Predictions
+historical.predictions <- all_team_data %>%
+    dplyr::select(team_date, team_team, team_opponent,
+                  team_win_chance, team_cumrate_win,
+                  team_upset_chance, team_cumrate_upset) %>%
+    rename("Date" = "team_date", "Team" = "team_team", "Opponent" = "team_opponent",
+           "Win Probability" = "team_win_chance", "Current Win Pct" = "team_cumrate_win", 
+           "Upset Probability" = "team_upset_chance", "Current Upset Pct" = "team_cumrate_upset") %>%
+    filter(Date != Sys.Date())
+
+#write_csv(historical.predictions, "/Users/camsmithers/Desktop/NBA Project/Main/Model Predictions/Game Outcome 2/historical_predictions.csv")
