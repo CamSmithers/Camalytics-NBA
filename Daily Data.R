5da2217082ab0a5a0ce71ba8f2c7af6256c07b5d
Cam <camalytics@outlook.com>
2025-01-12 14:06:38 -0500

    Jan 12th, 2025

--- Daily Data.R
+++ Daily Data.R
@@ -3,6 +3,80 @@ daily.predictions <- all_team_data %>%
     dplyr::select(team_date, team_team, team_opponent,
                   winloss_predicted_values, team_cumrate_win,
                   upset_predicted_values, team_cumrate_upset) %>%
+    mutate(team_team = case_when(
+        #Atlantic Division
+        team_team == "bos" ~ "Boston Celtics",
+        team_team == "bkn" ~ "Brooklyn Nets",
+        team_team == "nyk" ~ "New York Knicks",
+        team_team == "phi" ~ "Philadelphia 76ers",
+        team_team == "tor" ~ "Toronto Raptors",
+        #Central Division
+        team_team == "chi" ~ "Chicago Bulls",
+        team_team == "cle" ~ "Cleveland Cavaliers",
+        team_team == "det" ~ "Detroit Pistons",
+        team_team == "ind" ~ "Indiana Pacers",
+        team_team == "mil" ~ "Milwaukee Bucks",
+        #Southeast Division
+        team_team == "atl" ~ "Atlanta Hawks",
+        team_team == "cha" ~ "Charlotte Hornets",
+        team_team == "mia" ~ "Miami Heat",
+        team_team == "orl" ~ "Orlando Magic",
+        team_team == "was" ~ "Washington Wizards",
+        #Northwest Division
+        team_team == "den" ~ "Denver Nuggets",
+        team_team == "min" ~ "Minnesota Timberwolves",
+        team_team == "okc" ~ "Oklahoma City Thunder",
+        team_team == "por" ~ "Portland Trail Blazers",
+        team_team == "uta" ~ "Utah Jazz",
+        #Pacific Division
+        team_team == "gsw" ~ "Golden State Warriors",
+        team_team == "lac" ~ "Los Angeles Clippers",
+        team_team == "lal" ~ "Los Angeles Lakers",
+        team_team == "phx" ~ "Phoenix Suns",
+        team_team == "sac" ~ "Sacramento Kings",
+        #Southwest Division
+        team_team == "dal" ~ "Dallas Mavericks",
+        team_team == "hou" ~ "Houston Rockets",
+        team_team == "mem" ~ "Memphis Grizzlies",
+        team_team == "nop" ~ "New Orleans Pelicans",
+        team_team == "sas" ~ "San Antonio Spurs")) %>%
+    mutate(team_opponent = case_when(
+        #Atlantic Division
+        team_opponent == "bos" ~ "Boston Celtics",
+        team_opponent == "bkn" ~ "Brooklyn Nets",
+        team_opponent == "nyk" ~ "New York Knicks",
+        team_opponent == "phi" ~ "Philadelphia 76ers",
+        team_opponent == "tor" ~ "Toronto Raptors",
+        #Central Division
+        team_opponent == "chi" ~ "Chicago Bulls",
+        team_opponent == "cle" ~ "Cleveland Cavaliers",
+        team_opponent == "det" ~ "Detroit Pistons",
+        team_opponent == "ind" ~ "Indiana Pacers",
+        team_opponent == "mil" ~ "Milwaukee Bucks",
+        #Southeast Division
+        team_opponent == "atl" ~ "Atlanta Hawks",
+        team_opponent == "cha" ~ "Charlotte Hornets",
+        team_opponent == "mia" ~ "Miami Heat",
+        team_opponent == "orl" ~ "Orlando Magic",
+        team_opponent == "was" ~ "Washington Wizards",
+        #Northwest Division
+        team_opponent == "den" ~ "Denver Nuggets",
+        team_opponent == "min" ~ "Minnesota Timberwolves",
+        team_opponent == "okc" ~ "Oklahoma City Thunder",
+        team_opponent == "por" ~ "Portland Trail Blazers",
+        team_opponent == "uta" ~ "Utah Jazz",
+        #Pacific Division
+        team_opponent == "gsw" ~ "Golden State Warriors",
+        team_opponent == "lac" ~ "Los Angeles Clippers",
+        team_opponent == "lal" ~ "Los Angeles Lakers",
+        team_opponent == "phx" ~ "Phoenix Suns",
+        team_opponent == "sac" ~ "Sacramento Kings",
+        #Southwest Division
+        team_opponent == "dal" ~ "Dallas Mavericks",
+        team_opponent == "hou" ~ "Houston Rockets",
+        team_opponent == "mem" ~ "Memphis Grizzlies",
+        team_opponent == "nop" ~ "New Orleans Pelicans",
+        team_opponent == "sas" ~ "San Antonio Spurs")) %>%
     rename("Game Date"="team_date", "Team"="team_team", "Opponent"="team_opponent",
            "Win Probability"="winloss_predicted_values", "Current Win Pct"="team_cumrate_win", 
            "Upset Probability"="upset_predicted_values", "Current Upset Pct"="team_cumrate_upset") %>%
@@ -16,13 +90,8 @@ write_csv(daily.predictions,
 
 #Outcome 2: Historical Predictions
 historical.predictions <- all_team_data %>%
-    dplyr::select(team_date, team_team, team_opponent,
-                  winloss_predicted_values, team_cumrate_win,
-                  upset_predicted_values, team_cumrate_upset) %>%
-    rename("Game Date" = "team_date", "Team" = "team_team", "Opponent" = "team_opponent",
-           "Win Probability" = "winloss_predicted_values", "Current Win Pct" = "team_cumrate_win", 
-           "Upset Probability" = "upset_predicted_values", "Current Upset Pct" = "team_cumrate_upset") %>%
-    filter(`Game Date` != Sys.Date())
+    filter(team_date != Sys.Date()) %>%
+    
 
-write_csv(historical.predictions,
-          "/Users/camsmithers/Desktop/NBA Project/Main/Model Predictions/Game Outcome 2/historical_predictions.csv")
+#write_csv(historical.predictions,
+#          "/Users/camsmithers/Desktop/NBA Project/Main/Model Predictions/Game Outcome 2/historical_predictions.csv")
--- Data Checks.R
+++ Data Checks.R
@@ -1,5 +1,5 @@
 # Checking Data Script
 data_check <- all_team_data %>%
-    dplyr::select() %>%
-    filter( == )
+    dplyr::select(team_date, starts_with("league_cummean"))
+#    filter( == )
 View(data_check)
