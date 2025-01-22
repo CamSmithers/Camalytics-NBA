ffd36aa216bd562fc766433cfb13da4aca052f33
Cam <camalytics@outlook.com>
2025-01-21 22:44:00 -0500

    Jan 21st, 2025 pt2

--- Data Checks.R
+++ Data Checks.R
@@ -1,4 +1,5 @@
 # Checking Data Script
-data_check <- all_team_data %>%
-    dplyr::select(team_date, team_team, team_gametotal)
+data_check <- team_visual_data %>%
+    dplyr::select(team_date, team_team, team_opponent, 
+                  upset_predicted_values, opponent_upset_predicted_values)
 View(data_check)
